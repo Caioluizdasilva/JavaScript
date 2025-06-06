4939bd31104afca53fc0b45de4cc04afb8a4fd0f
Caio Luiz da Silva <caio.silva.3@aluno.cefet-rj.br>
2025-06-06 11:20:57 -0300

    add_exc2

---
+++ exercicio2.js
@@ -0,0 +1,13 @@
+// Verificar idade para retirar carteira de habilitação
+
+const idade = 17;
+
+if (idade >= 18) {
+  console.log('pode dirigir');
+} 
+else if (idade >= 17) {
+  console.log('pode procurar a auto escola');
+} 
+else {
+  console.log('abaixo da idade mínima');
+}
